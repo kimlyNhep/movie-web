4b8a39e468240a53674be7d7f21e46901e375d7d
kimlyNhep <kimly.nhep0168@gmail.com>
2021-06-20 16:29:15 +0700

    default movie picture

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -4,30 +4,30 @@ import {
   MailOutlined,
   UserOutlined,
   LoadingOutlined,
-} from '@ant-design/icons';
-import { Layout, Menu, Dropdown, Button } from 'antd';
-import cls from 'classnames';
-import { Navbar } from '../components/Navbar';
-import { LoginSvg } from '../components/svg';
-import { Footer } from '../components/Footer';
-import styles from './styles.module.css';
+} from "@ant-design/icons";
+import { Layout, Menu, Dropdown, Button } from "antd";
+import cls from "classnames";
+import { Navbar } from "../components/Navbar";
+import { LoginSvg } from "../components/svg";
+import { Footer } from "../components/Footer";
+import styles from "./styles.module.css";
 import {
   MeDocument,
   MeQuery,
   useLogoutMutation,
   useMeQuery,
   UserRoles,
-} from '../generated/graphql';
-import { useRouter } from 'next/router';
-import { Spin } from 'antd';
-import Cookie from 'js-cookie';
+} from "../generated/graphql";
+import { useRouter } from "next/router";
+import { Spin } from "antd";
+import Cookie from "js-cookie";
 
 interface ILayoutProps {
   classname?: string;
 }
 
 const MainLayout: React.FC<ILayoutProps> = ({ classname, children }) => {
-  const { data, loading } = useMeQuery({ fetchPolicy: 'cache-first' });
+  const { data, loading } = useMeQuery({ fetchPolicy: "cache-first" });
   const [logoutRequest] = useLogoutMutation();
   const router = useRouter();
 
@@ -73,18 +73,18 @@ const MainLayout: React.FC<ILayoutProps> = ({ classname, children }) => {
 
   const handleMenuClick = (value: any) => {
     switch (value.key) {
-      case 'login':
-        return router.push('/login');
-      case 'signup':
-        return router.push('/signup');
-      case 'logout':
+      case "login":
+        return router.push("/login");
+      case "signup":
+        return router.push("/signup");
+      case "logout":
         handleLogOut();
         break;
-      case 'create-new-character':
-        router.push('/signup/actor');
+      case "create-new-character":
+        router.push("/signup/actor");
         break;
       default:
-        return router.push('/');
+        return router.push("/");
     }
   };
 
@@ -94,12 +94,13 @@ const MainLayout: React.FC<ILayoutProps> = ({ classname, children }) => {
         cache.writeQuery<MeQuery>({
           query: MeDocument,
           data: {
+            __typename: "Query",
             me: null,
           },
         });
 
-        Cookie.remove('token');
-        router.push('/');
+        Cookie.remove("token");
+        router.push("/");
       },
     });
   };
@@ -114,21 +115,21 @@ const MainLayout: React.FC<ILayoutProps> = ({ classname, children }) => {
             Movie Academy
           </a>
           <div className={styles.action}>
-            <MailOutlined style={{ fontSize: '25px', paddingRight: '1rem' }} />
-            <BellOutlined style={{ fontSize: '25px', paddingRight: '1rem' }} />
-            <HeartOutlined style={{ fontSize: '25px', paddingRight: '1rem' }} />
+            <MailOutlined style={{ fontSize: "25px", paddingRight: "1rem" }} />
+            <BellOutlined style={{ fontSize: "25px", paddingRight: "1rem" }} />
+            <HeartOutlined style={{ fontSize: "25px", paddingRight: "1rem" }} />
             {data?.me?.username && (
               <Button
                 type='link'
                 href={`/profile/${data.me.id}`}
-                style={{ fontSize: '16px', paddingRight: '1rem' }}
+                style={{ fontSize: "16px", paddingRight: "1rem" }}
               >
                 {data?.me?.username}
               </Button>
             )}
             {loading && <Spin indicator={antIcon} />}
             <Dropdown overlay={menu} placement='bottomCenter' arrow>
-              <UserOutlined style={{ fontSize: '25px' }} />
+              <UserOutlined style={{ fontSize: "25px" }} />
             </Dropdown>
           </div>
         </div>
